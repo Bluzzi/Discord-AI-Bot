df507ef295972f31fdc0afbb32e41fff2453e2d6
Bluzzi <camilledugas.pro@gmail.com>
2025-12-29 11:13:26 +0100

    feat(process): add uncaught exception handler to log errors and exit process gracefully

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { env } from "#/utils/env";
 // Imports:
 import { discordClient } from "#/discord";
 import { logger } from "#/utils/logger";
+import { uncaughtExceptionHandler } from "#/utils/process";
+
+// Uncaught exception handler:
+uncaughtExceptionHandler();
 
 // Start bot:
 await discordClient.login(env.DISCORD_BOT_TOKEN);
