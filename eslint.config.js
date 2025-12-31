91ac62257ea8c86020b2e3d53899f25004947ad2
Bluzzi <camilledugas.pro@gmail.com>
2025-12-31 07:48:56 +0100

    chore(eslint): remove excessive rules and add ignores for specific files to streamline linting process

--- eslint.config.js
+++ eslint.config.js
@@ -5,31 +5,23 @@ export default eslintConfig(
     typescript: { tsconfigPath: "./tsconfig.json" },
   },
   {
-    files: ["src/tools/**/*.ts"],
-    rules: {
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/restrict-template-expressions": "off",
-      "@typescript-eslint/no-unnecessary-condition": "off",
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-      "@typescript-eslint/no-deprecated": "off",
-      "@typescript-eslint/no-unnecessary-type-conversion": "off",
-      "require-unicode-regexp": "off",
-      "func-style": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-floating-promises": "off",
-      "no-restricted-syntax": "off",
-      "no-plusplus": "off",
-      "no-constant-binary-expression": "off",
-    },
-  },
-  {
     rules: {
       "@typescript-eslint/no-misused-promises": "off",
     },
   },
+  {
+    ignores: [
+      "src/tools/discord.ts",
+      "src/tools/giphy.ts",
+      "src/tools/github.ts",
+      "src/tools/igdb.ts",
+      "src/tools/image.ts",
+      "src/tools/news.ts",
+      "src/tools/pastebin.ts",
+      "src/tools/pdf.ts",
+      "src/tools/school-42.ts",
+      "src/tools/steam.ts",
+      "src/tools/websearch.ts",
+    ],
+  }
 );
