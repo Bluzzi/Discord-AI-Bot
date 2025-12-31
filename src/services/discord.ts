4aff20149b006c61a3cfa2cba6ad46e354a9cc6e
Bluzzi <camilledugas.pro@gmail.com>
2025-12-31 12:55:16 +0100

    feat(discord.ts): integrate logger for improved logging of Discord bot startup events

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -1,4 +1,5 @@
 import { env } from "#/utils/env";
+import { logger } from "#/utils/logger";
 import { Client, GatewayIntentBits, Partials } from "discord.js";
 
 export const DISCORD_MAX_MESSAGE_LENGTH = 2000;
@@ -11,16 +12,18 @@ export const discordClient = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.DirectMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
   ],
   partials: [
     Partials.Channel,
     Partials.Message,
+    Partials.Reaction,
   ],
 });
 
 export const discordClientStart = async (loadEvents: () => Promise<void>) => {
   discordClient.once("clientReady", (readyClient) => {
-    console.log(`Discord bot successfully started!! Logged in as ${readyClient.user.tag}`);
+    logger.info(`Discord bot successfully started!! Logged in as ${readyClient.user.tag}`);
   });
 
   await loadEvents();
